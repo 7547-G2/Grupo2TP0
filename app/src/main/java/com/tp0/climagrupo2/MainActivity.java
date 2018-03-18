0cf6f6a98f955537f70c40fbacbf62edbea5c56b
Carlos Furnari <carlosfurnari@gmail.com>
2018-03-17 21:23:54 -0300

    updates on main screen

--- app/src/main/java/com/tp0/climagrupo2/MainActivity.java
+++ app/src/main/java/com/tp0/climagrupo2/MainActivity.java
@@ -16,17 +16,28 @@ import android.widget.Toast;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     int cityCode = 000;
     String BASE_URI = "http://api.openweathermap.org/data/2.5/forecast?";
-    List<Integer> tempList;
+    List<ResponseInfo> tempList = new ArrayList<>();
+
+    String mocker = "{\"id\": 707860,\"name\": \"Hurzuf\",\"country\": \"UA\",\"info\": [{\"date\": \"2018-03-18\",\"dayTemp\": 15,\"nightTemp\": 10},{\"date\": \"2018-03-19\",\"dayTemp\": 16, \"nightTemp\": 11}" +
+            ",{\"date\": \"2018-03-20\",\"dayTemp\": 17, \"nightTemp\": 12},{\"date\": \"2018-03-21\",\"dayTemp\": 18, \"nightTemp\": 13}" +
+            ",{\"date\": \"2018-03-22\",\"dayTemp\": 19, \"nightTemp\": 14}]}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +53,29 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
             String url = BASE_URI + "id=3435910&APPID=a7cdafb6e20c8ea2915a3c5bc16da0a3";
             volleyJsonObjectRequest(url);
-            loadTable();
+            loadTableDefault();
         }
 
+    }
+
+    private void loadTableDefault() {
+
+        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+
+        TableLayout tblAddLayout = (TableLayout) findViewById(R.id.tableLayout);
+        tblAddLayout.removeAllViews();
+        TextView dayName, tempDay, tempNight;
+
+        for (int i=0;i<5;i++){
+            View tr = inflater.inflate(R.layout.table_row, null);
+            dayName = (TextView) tr.findViewById(R.id.tvDayName);
+            dayName.setText("NNNN");
+            tempDay = (TextView) tr.findViewById(R.id.tvDayTemp);
+            tempDay.setText("XX" + "°C");
+            tempNight = (TextView) tr.findViewById(R.id.tvNightTemp);
+            tempNight.setText("XX" + "°C");
+            tblAddLayout.addView(tr);
+        }
     }
 
     public boolean isOnline() {
@@ -78,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("codigo: " + data.getDataString());
             String url = BASE_URI + "id=3435910&APPID=a7cdafb6e20c8ea2915a3c5bc16da0a3";
             volleyJsonObjectRequest(url);
-
             loadTable();
 
         }
@@ -90,18 +120,26 @@ public class MainActivity extends AppCompatActivity {
 
         TableLayout tblAddLayout = (TableLayout) findViewById(R.id.tableLayout);
         tblAddLayout.removeAllViews();
-        TextView tempDay, tempNight;
-
+        TextView dayName, tempDay, tempNight;
+        String day = "";
         for (int i=0;i<5;i++){
             View tr = inflater.inflate(R.layout.table_row, null);
+            dayName = (TextView) tr.findViewById(R.id.tvDayName);
+            switch (i) {
+                case 0: day = "Hoy";
+                        break;
+                case 1: day = "Mañana";
+                        break;
+                default: day = tempList.get(i).getDay();
+                         break;
+            }
+            dayName.setText(day);
             tempDay = (TextView) tr.findViewById(R.id.tvDayTemp);
-            tempDay.setText(String.valueOf(15+i) + "°C");
+            tempDay.setText(tempList.get(i).getDayTemp().toString() + "°C");
             tempNight = (TextView) tr.findViewById(R.id.tvNightTemp);
-            tempNight.setText(String.valueOf(10+i)+ "°C");
+            tempNight.setText(tempList.get(i).getNightTemp().toString() + "°C");
             tblAddLayout.addView(tr);
         }
-
-
     }
 
     public void volleyJsonObjectRequest(String url){
@@ -113,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         System.out.println(response.toString());
-                        parseResponse(response);
+                        parseResponse(mocker);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -125,11 +163,44 @@ public class MainActivity extends AppCompatActivity {
         AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectReq,REQUEST_TAG);
     }
 
-    private void parseResponse(JSONObject response) {
+    private void parseResponse(String response2) {
         try {
+            JSONObject response = new JSONObject(response2);
             String city = response.getString("name");
+            String country = response.getString("country");
+            TextView cityName = (TextView) findViewById(R.id.tvCity);
+            cityName.setText(city + ", " + country);
+            JSONArray jsonInfo = response.getJSONArray("info");
+            JSONObject aux;
+            tempList.clear();
+            String tmp, day;
+            for (int j=0;j<jsonInfo.length();j++){
+                aux = jsonInfo.getJSONObject(j);
+                tmp = aux.getString("date");
+                day = getDay(tmp);
+                day = day + ", " + tmp.substring(8, 10) + "/" + tmp.substring(5, 7);
+                ResponseInfo elem = new ResponseInfo(day, aux.getInt("dayTemp"), aux.getInt("nightTemp"));
+                tempList.add(j,elem);
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        System.out.println("cantidad: " + tempList.size());
     }
+
+    private String getDay(String input) {
+        SimpleDateFormat dateformat=new SimpleDateFormat("yyyy-MM-dd");
+        Date date;
+        String ret = "";
+        try {
+            date = dateformat.parse(input);
+            DateFormat dayFormate=new SimpleDateFormat("EEEE");
+            ret = dayFormate.format(date);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
 }
